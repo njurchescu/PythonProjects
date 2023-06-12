e241f8bb3b1e83bcfc050700eccccaff0d1524b1
njurchescu <68708760+njurchescu@users.noreply.github.com>
2023-06-12 16:25:55 +0300

    Add files via upload

---
+++ Day24/main.py
@@ -0,0 +1,58 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+#
+#
+# # with open("C:/Users/Home/Desktop/my_file.txt", mode="w") as file:
+# #     file.write("NICK INPUT")
+#
+
+
+
